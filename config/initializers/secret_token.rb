b7d5069ddf86835aea31ad97b402d536d0d2f592
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-03-04 16:29:20 +0530

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PossibleHabtmBug::Application.config.secret_token = '9e01057a4a1aa3e4302a5969e8d582b1f5deee0499056cb87af3b5be6b4e36387e6771a82bb82d71b3e3877088cb4a28c66bd40e96ee8fdc5d00a41f8bb0767b'
